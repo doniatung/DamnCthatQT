d4afb1c1d9612cd8339274bd93a779375ec3af31
Donia Tung <doniatung32@gmail.com>
2018-05-22 13:05:06 -0400

    organization

---
+++ appname.wsgi
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import sys
+
+sys.path.insert(0,"/var/www/appname/")
+
+from appname import app as application
--- utils/event.py
+++
@@ -1,7 +0,0 @@
-#event.py for accessing the EventBrite API
-import json
-
-#get the key
-f = open('../.secret_key.txt', 'rU')
-EVENTBRITE_KEY = json.loads(f.read())["eventbrite"]
-f.close()
