1340635946e10a6b07fcf39a3db2b31ca42e508a
Carol217 <cpan3@stuy.edu>
2018-05-23 12:56:13 -0400

    something got deleted

--- appname/__init__.py
+++ appname/__init__.py
@@ -6,7 +6,27 @@ P02 - Fin
 '''
 
 #initializing...
+from flask import Flask, render_template, request
+app = Flask("__name__")
 
+
+@app.route("/")
+def welcome():
+    return "WIP - check us out later ;)"
+
+@app.route("/signup")
+def create_acc():
+    return "WIP - check us out later ;)"
+
+@app.route("/login")
+def login():
+    return "WIP - check us out later ;)"
+
+@app.rout("/auth")
+def auth_acc():
+    return "WIP - check us out later ;)"
+
+''' que es?
     except KeyError:
         order = 'desc'
     if not (args['query'] or args['max_amt'] or len(cuisines) != 0):
@@ -23,8 +43,9 @@ P02 - Fin
             order)
     return render_template("zomato_results.html",
             rests = rests['restaurants'],
-            num = rests['results_shown'])
-
+    num = rests['results_shown'])
+    '''
+    
 if __name__ == "__main__":
     #when we change to lamp stack, change debug to False
     app.debug = True
