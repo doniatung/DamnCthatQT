f5487ca51b074bef0c2efeab993cc8f31ee936d8
Valery Chen <cpan3@stuy.edu>
2018-05-26 19:04:15 -0400

    db for accounts working

--- appname/utils/database.py
+++ appname/utils/database.py
@@ -20,13 +20,32 @@ add_interest(user,like) - adds user interest individually
 get_interest(user) - procures a tuple of all user interests
     returns (T/F, tuple) -- (did it work, interests)
 
-pair_up(user1, user2) - puts two user in a relationship
+NOTE: THE FOLLOWING METHODS HAVE NOT BEEN COMPLETED
+get_users() - gets all users
+    returns (list of users)
+
+start_plan(user1, user2, date) - adds the pairing of user1 and user2 into the db
+    returns (T/F, dateid)
+
+check_plans(user) - gets all the dates that user is planning from dates table
+    returns ((plans user starts),(plans user is invited to))
+
+confirm_plan(dateid, user) - invited user agrees to date
+    returns T/F
+
+add_event(dateid, event, location, time) - adds event to date
+    returns T/F
+
+get_plan(dateid) - gets all the events listed in the date
+
+add_image(dateid, imgfile, location) - adds image to db under appropriate event
     returns T/F
 '''
 
 global db
 import sqlite3
 import hashlib
+import time
 
 #open database
 def open_db():
@@ -53,16 +72,16 @@ def setup():
     #user likes (are dislikes necessary?)
     stmt= "CREATE TABLE IF NOT EXISTS likes(user TEXT, like TEXT, PRIMARY KEY(user,like))"
     c.execute(stmt)
-    #relationships
-    stmt= "CREATE TABLE IF NOT EXISTS ships(user1 TEXT, user2 TEXT, status INT, PRIMARY KEY(user1, user2))"
-    c.execute(stmt)
     #dates
-    stmt= "CREATE TABLE IF NOT EXISTS dates(dateid INT PRIMARY KEY, pairid INT, date TEXT, FOREIGN KEY(pairid) REFERENCES ships(rowid))"
+    stmt= "CREATE TABLE IF NOT EXISTS dates(dateid INT PRIMARY KEY, user1 TEXT, user2 TEXT, date TEXT, status INT)"
+    c.execute(stmt)
+    #planning
+    stmt = "CREATE TABLE IF NOT EXISTS planner(dateid INT, event TEXT, location TEXT,time TEXT)"
     c.execute(stmt)
     #images
-    stmt= "CREATE TABLE IF NOT EXISTS imgs(dateid INT, place TEXT, address TEXT, image BLOB, FOREIGN KEY(dateid) REFERENCES dates(dateid))"
+    stmt= "CREATE TABLE IF NOT EXISTS imgs(dateid INT, place TEXT, address TEXT, image BLOB)"
     c.execute(stmt)
-    
+
     close_db()
     return
 #=====================================
@@ -83,6 +102,7 @@ def create_acc(user, pwd1, pwd2, zipcode):
         c.execute(command, (user,hash_pwd,zipcode)) #try to see if user exists
         #if pwds don't match
         if pwd1 != pwd2:
+            print "passwords don't match"
             db.close() #don't commit and close
             return (True, False)
         else:
@@ -137,7 +157,7 @@ def add_interest(user, like):
         c.execute(command, (user,like)) #insert
         close_db()
     except:
-        print "Error: like already registered"
+        print "Error: like " + like + " already registered"
         return False
     
     return True
@@ -145,7 +165,7 @@ def add_interest(user, like):
 
 #GET INTEREST (FOR EVENTBRITE)
 #----------------------------------------
-def get_interest(user)
+def get_interest(user):
     global db
     try:
         user = user.strip().lower()
@@ -158,25 +178,16 @@ def get_interest(user)
         print "Error: could not make call on interest"
         return (False,)
     return (True, likes)
-#=========================================
-
-#PAIR UP
-#-----------------------------------------
-def pair_up(user1,user2)
-    global db
-    try:
-        user1 = user1.strip().lower()
-        user2 = user2.strip().lower()
-        c = open_db()
-
-        command = "INSERT INTO ships VALUES(?,?,1)" #one means relationship is 'on'
-        c.execute(command, (user1,user2))
-        c.execute(command, (user2,user1))
-        close_db()
-    except:
-        print "Relationship already exists - restarting"
-#==========================================     
+#=========================================  
         
-
-
 #setup()
+
+
+'''testing'''
+#print create_acc("crashley", "bubba", "bubba", 10000)
+#print auth("crashley", "bobba")
+'''
+likes = ["happiness", "butter", "butterflies", "good mac and cheese", "sing", "song", "sing"]
+for entry in likes:
+    add_interest("crashley", entry)
+'''
