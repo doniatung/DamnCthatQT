25f4eef9c4369362321a611e28095294e75263d3
Valery Chen <cpan3@stuy.edu>
2018-05-30 19:40:29 -0400

    eventbrite call

--- appname/utils/event.py
+++ appname/utils/event.py
@@ -5,3 +5,16 @@ import json
 f = open('../.secret_key.txt', 'rU')
 EVENTBRITE_KEY = json.loads(f.read())["eventbrite"]
 f.close()
+
+
+def get_events(like, zipcode):
+    url = "https://www.eventbriteapi.com/v3/events/search/?token=" \
+          + EVENTBRITE_KEY + "&q=" + like + "&location.address=" + zipcode\
+          + "&location.within=50mi"
+    response = urllib2.urlopen(url)
+    url = response.geturl()
+    info = response.read()
+    info = json.loads(info)
+    #print info
+    return info
+
